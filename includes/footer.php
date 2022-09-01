3ea0f8bffcbe7ebc34970bbc58777f24dffbf413
Deepika <palakondadeepika25@gmail.com>
2022-09-01 16:26:38 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+   <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Nitte Meenakshi Institute of technology <?php echo date('Y');?></div>
+                            <div>
+                                <a href="https://phpgurukul.com/" target="_blank">Palakonda Deepika Reddy</a>
+    
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
